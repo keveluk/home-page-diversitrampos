11f17ca48cc41681966a21c21e66f0942e991784
Keveluk <carlos.stefano@hotmail.com.br>
2021-11-08 09:06:51 -0300

    Criacao de sections sobre, valores e depoimentos

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function mobileMenu() {
     navMenu.classList.toggle("active");
 }
 hamburger.addEventListener("click", mobileMenu);
+
+const rightArrow = document.querySelector(".right-arrow");
+const leftArrow = document.querySelector(".left-arrow");
+const partnerList = document.querySelector(".partner-list");
+let slideCount =0;
+
+function slideLeft() {    
+    if(slideCount>=-600){
+        slideCount+=-240;
+        let move = slideCount.toString()+'px';
+        partnerList.style.transform=`translateX(${move})`;
+     }else{
+         slideCount = 0 ;
+         partnerList.style.transform=`translateX(0)`;
+     }
+    
+}
+function slideRight() {
+   if(slideCount<=600){
+        slideCount+=240;
+        let move = slideCount.toString()+'px';
+        partnerList.style.transform=`translateX(${move})`;
+     }else{
+         slideCount = 0 ;
+         partnerList.style.transform=`translateX(0)`;
+     }
+}
+leftArrow.addEventListener("click", slideLeft);
+rightArrow.addEventListener("click", slideRight);
